f8c51b9fe4875752eb6ee2f0eb097613fb8538f5
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 16:36:37 +0530

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,34 +1,42 @@
-
-export  default function appReducer(state, action ) {
-    console.log('desde appReducer', state, action)
-
-    switch (action.type) {
-      //
-      case "ADD_TASK":
-        return {
-          tasks: [...state.tasks,  action.payload],
-        };
-        //
-      case "DELETE_TASK":
-  //  console.log(action.payload)     
-        return {
-          tasks: state.tasks.filter(task => task.id !== action.payload)
-        };
-        //
-    
-       //
-       case 'TOGGLE_TASK_DONE':
-       return {
-         tasks: state.tasks.map((task) =>
-           task.id === action.payload ? {...task , done: !task.done} : task
-         ),
-       };
-
-      default:
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case "DELETE_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+    case "TOGGLE_TASK_DONE":
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload ? { ...task, done: !task.done } : task
+        ),
+      };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
+      };
+    case "UPDATE_QUANTITY":
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) =>
+          item.id === action.payload.id ? { ...item, quantity: action.payload.quantity } : item
+        ),
+      };
+    default:
       return state;
-    }
-}
+  }
+};
 
-
-
-
+export default appReducer;

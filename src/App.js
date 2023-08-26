c75c9141690bfb90fb113efba1c4a10bad63a30a
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 16:44:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import { ContextProvider } from "./context/ContextProvider";
-import { CartProvider } from "./context/CartContext"; // Import CartProvider
 import TaskList from "./pages/TaskList";
 
 function App() {
   return (
-    <CartProvider>
-      <div className="h-screen text-white text-center p-10">
-        <div className="container mx-auto h-full">
-          <ContextProvider>
-            <TaskList />
-          </ContextProvider>
-        </div>
+    <div className="h-screen text-white text-center p-10">
+      <div className="container mx-auto h-full">
+        <ContextProvider>
+          <TaskList />
+        </ContextProvider>
       </div>
-    </CartProvider>
+    </div>
   );
 }
 

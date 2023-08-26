71918c28892f67f090dbcb4a7d1def9405d758b6
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 16:12:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import { ContextProvider } from './context/ContextProvider';
+import React from "react";
+import { ContextProvider } from "./context/ContextProvider";
 import TaskList from "./pages/TaskList";
-
- 
-
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <div className="h-screen text-white text-center p-10">
         <div className="container mx-auto h-full">
           <ContextProvider>
@@ -15,7 +13,7 @@ function App() {
           </ContextProvider>
         </div>
       </div>
-    </>
+    </CartProvider>
   );
 }
 

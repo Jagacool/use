56f1adeb64eae6c65abc986e9dc217c3586e7662
j <137029997+Jagacool@users.noreply.github.com>
2023-08-26 16:41:59 +0530

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,42 +1,46 @@
-const appReducer = (state, action) => {
+export default function appReducer(state, action) {
   switch (action.type) {
     case "ADD_TASK":
-      return {
-        ...state,
-        tasks: [...state.tasks, action.payload],
-      };
+      // Existing logic for adding tasks
+      return { ...state, tasks: [...state.tasks, action.payload] };
+
     case "DELETE_TASK":
-      return {
-        ...state,
-        tasks: state.tasks.filter((task) => task.id !== action.payload),
-      };
+      // Existing logic for deleting tasks
+      return { ...state, tasks: state.tasks.filter(task => task.id !== action.payload) };
+
     case "TOGGLE_TASK_DONE":
+      // Existing logic for toggling task done
       return {
         ...state,
-        tasks: state.tasks.map((task) =>
+        tasks: state.tasks.map(task =>
           task.id === action.payload ? { ...task, done: !task.done } : task
         ),
       };
+
     case "ADD_TO_CART":
+      // Add the product to the cartItems array
       return {
         ...state,
         cartItems: [...state.cartItems, action.payload],
       };
+
     case "REMOVE_FROM_CART":
+      // Remove the product from the cartItems array
       return {
         ...state,
-        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
+        cartItems: state.cartItems.filter(item => item.id !== action.payload),
       };
+
     case "UPDATE_QUANTITY":
+      // Update the quantity of a specific product in the cartItems array
       return {
         ...state,
-        cartItems: state.cartItems.map((item) =>
+        cartItems: state.cartItems.map(item =>
           item.id === action.payload.id ? { ...item, quantity: action.payload.quantity } : item
         ),
       };
+
     default:
       return state;
   }
-};
-
-export default appReducer;
+}
